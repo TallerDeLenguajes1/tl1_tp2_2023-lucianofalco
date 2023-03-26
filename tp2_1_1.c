ecae14739ce8fc8d2bf817cdcdb88a59f97ecc83
Lucianofalco <lucianosebastianfalco@gmail.com>
2023-03-25 15:03:15 -1200

    first commit

---
+++ tp2_1_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 20
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    double vt[N];
+    for (i = 0; i < N; i++)
+    {
+        vt[i] = 1 + rand() % 100;
+        printf("%f", vt[i]);
+    }
+
+    return 0;
+}
